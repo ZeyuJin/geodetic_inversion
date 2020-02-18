a52a1119c758bb869cdc796506a42add8676e837
ZeyuJin <zej011@ucsd.edu>
2020-02-18 14:38:59 -0800

    fix the column vectors

--- geometry/compute_patch_coords.m
+++ geometry/compute_patch_coords.m
@@ -3,6 +3,7 @@ function [x_center,y_center,z_center] = compute_patch_coords(xp,yp,zp,lp,wp,stri
 % X,Y start from the strike direction, Z start from the top
 % lp,wp mean the length and width of this patch
 % the function will compute the central coordinates of this patch
+% solve the bug of coordinates as a column vector
     d2r = pi / 180;
     p_theta = (90 - strike) * d2r;
     p_dip = dip * d2r;
@@ -13,9 +14,9 @@ function [x_center,y_center,z_center] = compute_patch_coords(xp,yp,zp,lp,wp,stri
     y2f = y1f - wp .* cos(p_dip);
     z2f = z1f - wp .* sin(p_dip);
     
-    xco = mean([x1f,x2f]);
-    yco = mean([y1f,y2f]);
-    zco = mean([z1f,z2f]);
+    xco = mean([x1f,x2f],2);
+    yco = mean([y1f,y2f],2);
+    zco = mean([z1f,z2f],2);
     
     [x_center,y_center] = xy2XY(xco,yco,-p_theta);
     z_center = zco;
