6cb9b7a26c9fc78e39fb005ea4f0d392b9e4ce73
Xiaoyu Zou <103137212+x3zou@users.noreply.github.com>
2024-03-21 16:55:20 -0700

    Add files via upload

--- sampling/resamp_insar_data.m
+++ sampling/resamp_insar_data.m
@@ -1,8 +1,15 @@
-function resamp_insar_data(data_list,Nmin,Nmax,iter_step,varargin)
+function resamp_insar_data(slip_model_in, data_list,Nmin,Nmax,iter_step,varargin)
 % resample the insar data with model predictions 
 % method in Kang Wang and Yuri Fialko, GRL 2015
 % written by Zeyu Jin on Sept. 2019
    set(0,'defaultAxesFontSize',15);
+   
+   % data_list defines each Interferogram track with such a format
+   % the number means the number of data points to be resampled
+   % ASC99    1200
+   % DES106   1500
+   
+   % slip_model_in is the previous inverted model
    
    %% DEFINE THE DEFAULT VALUES
    iint=iter_step;
@@ -57,13 +64,16 @@ function resamp_insar_data(data_list,Nmin,Nmax,iter_step,varargin)
        count = count + 1;
        strs = strsplit(tmp_txt);
        track(count) = cellstr(strs{1});
+       disp(count);
+       disp(npt);
+       disp(str2double(strs{2}));
        npt(count) = str2double(strs{2});  
        tmp_txt = fgetl(fid);
    end     
    fclose(fid);
-%    [xo,yo] = utm2ll(lon_eq,lat_eq,0,1);
    [xo,yo] = ll2xy(lon_eq,lat_eq,ref_lon);
    
+   % % data covariance matrix
    % varigram=load('insar_varigram.mat');
    % sigma=varigram.sigma;
    % L=varigram.L;
@@ -74,7 +84,7 @@ function resamp_insar_data(data_list,Nmin,Nmax,iter_step,varargin)
        disp(['working on ',this_track]);
        this_npt=npt(k);
        
-       [x1,y1,demin]=grdread2([this_track,'/','dem_low.grd']);
+       % [x1,y1,demin]=grdread2([this_track,'/','dem_low.grd']);
        [x1,y1,losin]=grdread2([this_track,'/','los_ll_low','.grd']);   % in the unit of cm
        [x1,y1,ze]=grdread2([this_track,'/','look_e_low','.grd']);
        [x1,y1,zn]=grdread2([this_track,'/','look_n_low','.grd']);
@@ -82,7 +92,7 @@ function resamp_insar_data(data_list,Nmin,Nmax,iter_step,varargin)
        
        % multi-look to reduce the computation time
        if Nlook > 1
-          [lon1,lat1,deml] = multi_look(x1,y1,demin,Nlook,Nlook);
+          % [lon1,lat1,deml] = multi_look(x1,y1,demin,Nlook,Nlook);
           [lon1,lat1,losl] = multi_look(x1,y1,losin,Nlook,Nlook);
           [lon1,lat1,zel] = multi_look(x1,y1,ze,Nlook,Nlook);
           [lon1,lat1,znl] = multi_look(x1,y1,zn,Nlook,Nlook);
@@ -98,17 +108,18 @@ function resamp_insar_data(data_list,Nmin,Nmax,iter_step,varargin)
        end
        
        [xm1,ym1] = meshgrid(lon1,lat1);
-%        [xutm,yutm] = utm2ll(xm1(:),ym1(:),0,1);
        [xutm,yutm] = ll2xy(xm1(:),ym1(:),ref_lon);
        xin = xutm - xo;
        yin = yutm - yo;
        xin = reshape(xin,size(xm1));
-       yin = reshape(yin,size(ym1));       
+       yin = reshape(yin,size(ym1));
+       
+       % % put the slip model as an input parameter
 %        slip_model_in = load('fault_M7.slip');
 %        slip_model_in = load('fault_M6.slip');
 %        tmp = load('resample/4_segments/homo_4data.mat');
-       tmp = load('resample/misfit_include_MAI/homo_better_data.mat');
-       slip_model_in = tmp.slip_model;
+%        tmp = load('resample/misfit_include_MAI/homo_better_data.mat');
+%        slip_model_in = tmp.slip_model;
        
        if strcmp(data_type,'insar')
           los_model = slip2insar_okada(xin,yin,losl,zel,znl,zul,slip_model_in);   % fix the bug using multi-looked looking angles
@@ -116,14 +127,20 @@ function resamp_insar_data(data_list,Nmin,Nmax,iter_step,varargin)
           los_model = slip2AZO_okada(xin,yin,losl,zel,znl,zul,slip_model_in);     % add module to compute AZO data
        end
        
-   %     Nmin = 3;   Nmax = 150;
+       % resample the data using slip model
+       % a bit similar to EM algorithm to make it diverge (E-step??  M-step?? Currently I don't know...)
+       disp(size(lon1));
+       disp(size(lat1));
+       disp(size(los_model));
        [lon_model,lat_model,zout_model,Npt,rms_out,xx1,xx2,yy1,yy2]=make_insar_downsample(lon1,lat1,los_model,this_npt,Nmin,Nmax,'mean'); % same with downsample
+
        [lon_pt,lat_pt,zout]=make_look_downsample(lon1,lat1,losl,lon_model,lat_model,xx1,xx2,yy1,yy2);
-       [lon_pt,lat_pt,dem_out]=make_look_downsample(lon1,lat1,deml,lon_model,lat_model,xx1,xx2,yy1,yy2);
+       % [lon_pt,lat_pt,dem_out]=make_look_downsample(lon1,lat1,deml,lon_model,lat_model,xx1,xx2,yy1,yy2);
        [lon_pt,lat_pt,ve]=make_look_downsample(lon1,lat1,zel,lon_model,lat_model,xx1,xx2,yy1,yy2);
        [lon_pt,lat_pt,vn]=make_look_downsample(lon1,lat1,znl,lon_model,lat_model,xx1,xx2,yy1,yy2);
        [lon_pt,lat_pt,vz]=make_look_downsample(lon1,lat1,zul,lon_model,lat_model,xx1,xx2,yy1,yy2);
-%        [xutm,yutm]=utm2ll(lon_pt,lat_pt,0,1);
+       
+       % convert lon/lat to x/y
        [xutm,yutm]=ll2xy(lon_pt,lat_pt,ref_lon);
        xpt=xutm-xo;
        ypt=yutm-yo;
@@ -132,7 +149,7 @@ function resamp_insar_data(data_list,Nmin,Nmax,iter_step,varargin)
        xpt=xpt(indx_good);
        ypt=ypt(indx_good);
        zout=zout(indx_good);
-       dem_out=dem_out(indx_good);
+       % dem_out=dem_out(indx_good);
        ve=ve(indx_good);
        vn=vn(indx_good);
        vz=vz(indx_good);
@@ -146,11 +163,11 @@ function resamp_insar_data(data_list,Nmin,Nmax,iter_step,varargin)
    %     this_L=L(k);
    %     covd = calc_insar_cov(xpt,ypt,this_sig,this_L); 
    %     save([this_track,'/','los_samp',num2str(iint),'.mat'],'insar_data','covd');
-       save([this_track,'/los_samp',num2str(iint),'.mat'],'sampled_insar_data','rms_out','dem_out');
+       save([this_track,'/los_samp',num2str(iint),'.mat'],'sampled_insar_data','rms_out');
        [hf,h1,h2]=plot_insar_sample_new(x1,y1,losin,zout,xx1,xx2,yy1,yy2,'fault',fault_file);
        set(hf,'PaperPositionMode','auto');
 %        set(hf,'visible','off');
-       saveas(hf,[this_track,'/','los_samp',num2str(iint)],'epsc');
+%       saveas(hf,[this_track,'/','los_samp',num2str(iint)],'epsc');
    end
    
 end
